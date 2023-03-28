c70bf93c046383c308a7ded8ddb79f60e028d5dd
Strauberg <stefanstrauberg@gmail.com>
2023-03-28 09:54:06 +0300

    Adding migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,5 +25,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+var scope = app.Services.CreateAsyncScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
